4609a54734762d262eaa989be48706a383d16060
Saksham Goyal <saksham@saksham.dev>
2023-07-17 20:17:22 -0500

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,11 @@
 <script>
-	// @ts-ignore
-	import Repos from "$lib/data.yaml";
-	import Test from "$lib/test.md";
-	function b64_to_utf8(str) {
-    const decoded = atob(str.split("base64,")[1])
-    console.log("decoded", decoded)
-		return decoded;
-	}
-	const ymlText = b64_to_utf8(Repos);
-	const markdown = b64_to_utf8(Test);
+	import SvelteMarkdown from 'svelte-markdown';
+  import Repos from "$lib/repos.yaml";
+  import Test from "$lib/test.md";
+
+	const ymlText = atob(Repos.split("base64,")[1]);
+	const markdown = atob(Test.split("base64,")[1]);
 </script>
 
 <p style="white-space: pre-wrap;">Text: {ymlText}</p>
-<p>Markdown: {markdown}</p>
+<SvelteMarkdown source={markdown} />
