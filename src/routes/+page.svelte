186a8a18c0b3482d026db7b72f9df41042ab86ff
Saksham Goyal <saksham@saksham.dev>
2023-07-18 18:31:58 -0500

    modal test working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,15 @@
 	import YAML from "yaml";
 	import Repos from "$lib/repos.yaml";
 	import Test from "$lib/markdowns/about.md";
+	import Modal from "$lib/comps/Modal_v2/Modal.svelte";
 
+	let showModal = false;
+
+	// console.log("Repos: ", Repos);
+	console.log("Repos split: ", String(Repos.split("base64,")[1]));
 	const ymlText = YAML.parse(atob(Repos.split("base64,")[1]));
 	const yamlJson = JSON.stringify(ymlText, null, 2);
 	const markdown = atob(Test.split("base64,")[1]);
-
-	for (const [itemNum, item] of Object.entries(ymlText)) {
-		// console.log(item);
-	}
 
 	// !!!! TODO !!!!
 	// configure tailwind template paths // https://tailwindcss.com/docs/guides/sveltekit
@@ -18,10 +19,15 @@
 	// configure tailwind +layout.svelte file // https://tailwindcss.com/docs/guides/sveltekit
 </script>
 
+<button on:click={() => (showModal = true)}> show modal </button>
+<Modal bind:showModal>
+	<SvelteMarkdown source={markdown} />
+</Modal>
+
 <!-- <p style="white-space: pre-wrap;">Text: {yamlJson}</p> -->
 <div>
 	{#each Object.entries(ymlText) as [itemNum, item]}
-		<pre class="boxed"><img src="https://raw.githubusercontent.com/{Object.values(item)[1].split('github.com/')[1]}/main/hero.png" alt="" />
+		<pre class="boxed"><img src="https://raw.githubusercontent.com/{Object.values(item)[1].split('github.com/')[1]}/main/hero.png" alt="" width="200px"/>
 			{Object.keys(item)[0]} :
 			{Object.keys(item)[1]} : {Object.values(item)[1]}
 			{Object.keys(item)[2]} : {Object.values(item)[2]}
@@ -30,7 +36,7 @@
 	{/each}
 </div>
 
-<SvelteMarkdown source={markdown} />
+
 
 <style>
 	.boxed {
