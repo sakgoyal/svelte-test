ef899bc81be82ea365809ba4154ac17743bc2178
Saksham Goyal <saksham@saksham.dev>
2023-07-17 23:13:30 -0500

    copy files from nextjs site before migration

---
+++ src/lib/pages/_app.tsx
@@ -0,0 +1,15 @@
+import '@/styles/globals.css'
+import Navigation from "@/comps/Nav/Nav.jsx"
+import Footer from "@/comps/Footer/Footer.jsx"
+
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Navigation />
+      <Component {...pageProps} className = "h-100"/>
+      <Footer />
+    </div>
+  )
+}
