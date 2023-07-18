3e364d5c0638243c1f2489409a38741323a3612a
Saksham Goyal <saksham@saksham.dev>
2023-07-17 19:50:33 -0500

    display correctly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,13 @@
 	import Repos from "$lib/data.yaml";
 	import Test from "$lib/test.md";
 	function b64_to_utf8(str) {
-		return (atob(str.split("base64,")[1]));
+    const decoded = atob(str.split("base64,")[1])
+    console.log("decoded", decoded)
+		return decoded;
 	}
 	const ymlText = b64_to_utf8(Repos);
 	const markdown = b64_to_utf8(Test);
 </script>
 
-<p>Text: {ymlText}</p>
+<p style="white-space: pre-wrap;">Text: {ymlText}</p>
 <p>Markdown: {markdown}</p>
